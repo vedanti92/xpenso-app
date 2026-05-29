17e033c87ef436ac75e6e90834e0cad9db41406f
vedanti <vedanti.r.bagade@gmail.com>
2026-05-29 15:08:47 +0530

    file updated

--- backend/src/main/java/com/example/xpenso/controller/FilterController.java
+++ backend/src/main/java/com/example/xpenso/controller/FilterController.java
@@ -29,7 +29,7 @@ public class FilterController {
         LocalDate startDate = filter.getStartDate() != null ? filter.getStartDate() : LocalDate.MIN;
         LocalDate endDate = filter.getEndDate() != null ? filter.getEndDate() : LocalDate.now();
         String keyword = filter.getKeyword() != null ? filter.getKeyword() : " ";
-        String sortField = filter.getSortField() != null ? filter.getSortField() : "date";
+        String sortField = (filter.getSortField() != null && !filter.getSortField().isEmpty()) ? filter.getSortField() : "date";
         Sort.Direction direction = "desc".equalsIgnoreCase(filter.getSortOrder()) ? Sort.Direction.DESC : Sort.Direction.ASC;
         Sort sort = Sort.by(direction, sortField);
         if ("income".equals(filter.getType())) {
